cf407d3b9e9abfeb471e0df74f10c22db32544aa
bjh7242 <bjh7242@rit.edu>
2015-12-05 18:08:54 -0500

    fixed dtoverlay command to update /boot/config.txt

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cd ..
 gpio mode 0 out
 gpio mode 1 out
 # enables gpio device at boot
-`dtoverlay=w1-gpio | sh`
+echo "dtoverlay=w1-gpio" >> /boot/config.txt
 sudo modprobe w1-gpio
 sudo modprobe w1-therm
 sudo apt-get -y install python-dev python-pip
