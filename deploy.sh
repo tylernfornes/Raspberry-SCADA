cf7a653ec0dae9b0f65b3dfb5d99183f77f43b5e
bjh7242 <bjh7242@rit.edu>
2015-12-05 18:04:27 -0500

    added python-pip and changed dtoverlay command

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ cd ..
 gpio mode 0 out
 gpio mode 1 out
 # enables gpio device at boot
-`exec dtoverlay=w1-gpio`
+`dtoverlay=w1-gpio | sh`
 sudo modprobe w1-gpio
 sudo modprobe w1-therm
-sudo apt-get install python-dev
+sudo apt-get -y install python-dev python-pip
 sudo pip install pymodbus Twisted pycrypto
