f1e8d783db4dc75ee5d6029f5873a6dfde9554cb
bjh7242 <bjh7242@rit.edu>
2015-12-05 17:45:46 -0500

    updated deploy script to support jesse light raspbian

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,14 @@
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+git pull origin
+./build
+cd ..
+# initialize the registers to be output registers
+gpio mode 0 out
+gpio mode 1 out
+# enables gpio device at boot
+`exec dtoverlay=w1-gpio`
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
 sudo apt-get install python-dev
 sudo pip install pymodbus Twisted pycrypto
